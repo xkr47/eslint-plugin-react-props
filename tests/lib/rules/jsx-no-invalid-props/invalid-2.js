4ead2b7a28cf4f263d37f014fa279d400b441f83
Jonas Berlin <jonas.berlin@nitorcreations.com>
2017-03-08 23:59:24 +0200

    Extract test code to separate files

---
+++ tests/lib/rules/jsx-no-invalid-props/invalid-2.js
@@ -0,0 +1,38 @@
+/* eslint import/no-extraneous-dependencies: "off", import/no-unresolved: "off" */
+/* eslint comma-dangle: "off", no-unused-vars: "off", object-curly-spacing: "off" */
+/* eslint import/extensions: "off", import/newline-after-import: "off" */
+
+import React, {PropTypes} from 'react';
+const TestComponent = {};
+TestComponent.propTypes = {
+  a: PropTypes.array,
+  b: PropTypes.bool,
+  c: PropTypes.func,
+  d: PropTypes.number,
+  e: PropTypes.object,
+  f: PropTypes.string,
+  g: PropTypes.any,
+  h: PropTypes.arrayOf,
+  i: PropTypes.element,
+  j: PropTypes.instanceOf,
+  k: PropTypes.node,
+  l: PropTypes.objectOf,
+  m: PropTypes.oneOf,
+  n: PropTypes.oneOfType,
+  o: PropTypes.shape,
+  p: PropTypes.array,
+  q: PropTypes.bool,
+  r: PropTypes.func,
+  s: PropTypes.number,
+  t: PropTypes.object,
+  u: PropTypes.string,
+  v: PropTypes.any,
+  w: PropTypes.arrayOf,
+  x: PropTypes.element,
+  y: PropTypes.instanceOf,
+  z: PropTypes.node,
+  aa: PropTypes.objectOf,
+  ab: PropTypes.oneOf,
+  ac: PropTypes.oneOfType,
+  ad: PropTypes.shape
+};
---
+++ tests/lib/rules/jsx-no-invalid-props/invalid-4.js
@@ -0,0 +1,9 @@
+/* eslint import/no-extraneous-dependencies: "off", import/no-unresolved: "off" */
+/* eslint comma-dangle: "off", no-unused-vars: "off", object-curly-spacing: "off" */
+/* eslint import/extensions: "off", import/newline-after-import: "off" */
+
+import React, {PropTypes} from 'react';
+const TestComponent = {};
+TestComponent.propTypes = {
+  a: PropTypes,
+};
